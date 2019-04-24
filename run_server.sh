91f62d059981e8bf49b0c5d08ca06d986d569bf0
Kevin <kevindevos9542@gmail.com>
2019-04-24 17:45:04 +0100

    Readd scripts

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,11 @@
+if [ $# -ne 1 ]; then
+	echo "Invalid number of commandline arguments. Pass the aws classpath directory as the first argument without ending '/' character"
+	exit 1
+fi
+
 export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
 
-java -cp build:$AWS_SDK_CLASSPATH WebServer
+CP=$(echo lib/*.jar | tr ' ' ':'):$(echo $1/lib/*.jar | tr ' ' ':'):$(echo $1/third-party/lib/*.jar | tr ' ' ':'):build
+
+java -cp $CP pt.ulisboa.tecnico.cnv.webserver.WebServer
 
