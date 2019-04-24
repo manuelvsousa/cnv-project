ce7bee59e1bda0e49805a66835f2e7982d556972
cintixx <cintia.almeida@tecnico.ulisboa.pt>
2019-04-24 09:17:32 -0700

    update codigo de instrumentacao dinamica

--- DynamicTool.java
+++ DynamicTool.java
@@ -1,4 +1,3 @@
-package pt.ulisboa.tecnico.cnv.webserver;
 
 import BIT.highBIT.*;
 import java.io.File;
@@ -28,10 +27,18 @@ public class DynamicTool{
 	private static int fieldloadcount = 0;
 	private static int fieldstorecount = 0;
 
+	private static StatisticsBranch[] branch_info;
+
 	private static int branch_number;
 	private static int branch_pc;
 	private static String branch_class_name;
 	private static String branch_method_name;
+
+	private static String class_name_;
+	private static String method_name_;
+	private static int pc_;
+	private static int taken_;
+	private static int not_taken_;
 
 
 
@@ -68,10 +75,13 @@ public class DynamicTool{
 	
     public static synchronized void printDynamic(String foo) 
 		{
+						System.out.println("Dynamic information summary:");
+
 			try{
 				 PrintWriter writer = new PrintWriter("met"+Thread.currentThread().getId()+".txt", "UTF-8");
 
-			System.out.println("Dynamic information summary:");
+						System.out.println("metodos" + dyn_method_count);
+
 			writer.println("Number of methods:      " + dyn_method_count);
 			writer.println("Number of basic blocks: " + dyn_bb_count);
 			writer.println("Number of instructions: " + dyn_instr_count);
@@ -87,6 +97,8 @@ public class DynamicTool{
 			writer.println("Average number of instructions per basic block: " + instr_per_bb);
 			writer.println("Average number of instructions per method:      " + instr_per_method);
 			writer.println("Average number of basic blocks per method:      " + bb_per_method);
+			writer.close();
+
 
 			}catch(FileNotFoundException|UnsupportedEncodingException e){
 				e.printStackTrace();
@@ -97,6 +109,8 @@ public class DynamicTool{
 
     public static synchronized void dynInstrCount(int incr) 
 		{
+		//System.out.println(dyn_instr_count);
+
 			dyn_instr_count += incr;
 			dyn_bb_count++;
 		}
@@ -107,68 +121,6 @@ public class DynamicTool{
 		}
 
 
-	public static void doLoadStore(File in_dir, File out_dir) 
-		{
-			String filelist[] = in_dir.list();
-			
-			for (int i = 0; i < filelist.length; i++) {
-				String filename = filelist[i];
-				if (filename.endsWith(".class")) {
-					String in_filename = in_dir.getAbsolutePath() + System.getProperty("file.separator") + filename;
-					String out_filename = out_dir.getAbsolutePath() + System.getProperty("file.separator") + filename;
-					ClassInfo ci = new ClassInfo(in_filename);
-
-					for (Enumeration e = ci.getRoutines().elements(); e.hasMoreElements(); ) {
-						Routine routine = (Routine) e.nextElement();
-						
-						for (Enumeration instrs = (routine.getInstructionArray()).elements(); instrs.hasMoreElements(); ) {
-							Instruction instr = (Instruction) instrs.nextElement();
-							int opcode=instr.getOpcode();
-							if (opcode == InstructionTable.getfield)
-								instr.addBefore("StatisticsTool", "LSFieldCount", new Integer(0));
-							else if (opcode == InstructionTable.putfield)
-								instr.addBefore("StatisticsTool", "LSFieldCount", new Integer(1));
-							else {
-								short instr_type = InstructionTable.InstructionTypeTable[opcode];
-								if (instr_type == InstructionTable.LOAD_INSTRUCTION) {
-									instr.addBefore("StatisticsTool", "LSCount", new Integer(0));
-								}
-								else if (instr_type == InstructionTable.STORE_INSTRUCTION) {
-									instr.addBefore("StatisticsTool", "LSCount", new Integer(1));
-								}
-							}
-						}
-					}
-					ci.addAfter("StatisticsTool", "printLoadStore", "null");
-					ci.write(out_filename);
-				}
-			}	
-		}
-
-	public static synchronized void printLoadStore(String s) 
-		{
-			System.out.println("Load Store Summary:");
-			System.out.println("Field load:    " + fieldloadcount);
-			System.out.println("Field store:   " + fieldstorecount);
-			System.out.println("Regular load:  " + loadcount);
-			System.out.println("Regular store: " + storecount);
-		}
-
-	public static synchronized void LSFieldCount(int type) 
-		{
-			if (type == 0)
-				fieldloadcount++;
-			else
-				fieldstorecount++;
-		}
-
-	public static synchronized void LSCount(int type) 
-		{
-			if (type == 0)
-				loadcount++;
-			else
-				storecount++;
-		}
 
 	public static void tryDynamic(File in_dir, File out_dir){
 
@@ -188,6 +140,7 @@ public class DynamicTool{
 
 					if (in_dir.isDirectory() && out_dir.isDirectory()) {
 						doDynamic(in_dir, out_dir);
+						doBranch(in_dir, out_dir);
 					}
 					else {
 						printUsage();
@@ -201,4 +154,142 @@ public class DynamicTool{
 			public static void printUsage() 
 		{
 			System.out.println("Syntax: tryDynamic([in_path] ,[out_path])");}
+
+		public static void doBranch(File in_dir, File out_dir) 
+		{
+			String filelist[] = in_dir.list();
+			int k = 0;
+			int total = 0;
+			
+			for (int i = 0; i < filelist.length; i++) {
+				String filename = filelist[i];
+				if (filename.endsWith(".class")) {
+					String in_filename = in_dir.getAbsolutePath() + System.getProperty("file.separator") + filename;
+					ClassInfo ci = new ClassInfo(in_filename);
+
+					for (Enumeration e = ci.getRoutines().elements(); e.hasMoreElements(); ) {
+						Routine routine = (Routine) e.nextElement();
+						InstructionArray instructions = routine.getInstructionArray();
+						for (Enumeration b = routine.getBasicBlocks().elements(); b.hasMoreElements(); ) {
+							BasicBlock bb = (BasicBlock) b.nextElement();
+							Instruction instr = (Instruction) instructions.elementAt(bb.getEndAddress());
+							short instr_type = InstructionTable.InstructionTypeTable[instr.getOpcode()];
+							if (instr_type == InstructionTable.CONDITIONAL_INSTRUCTION) {
+								total++;
+							}
+						}
+					}
+				}
+			}
+			
+			for (int i = 0; i < filelist.length; i++) {
+				String filename = filelist[i];
+				if (filename.endsWith(".class")) {
+					String in_filename = in_dir.getAbsolutePath() + System.getProperty("file.separator") + filename;
+					String out_filename = out_dir.getAbsolutePath() + System.getProperty("file.separator") + filename;
+					ClassInfo ci = new ClassInfo(in_filename);
+
+					for (Enumeration e = ci.getRoutines().elements(); e.hasMoreElements(); ) {
+						Routine routine = (Routine) e.nextElement();
+						routine.addBefore("DynamicTool", "setBranchMethodName", routine.getMethodName());
+						InstructionArray instructions = routine.getInstructionArray();
+						for (Enumeration b = routine.getBasicBlocks().elements(); b.hasMoreElements(); ) {
+							BasicBlock bb = (BasicBlock) b.nextElement();
+							Instruction instr = (Instruction) instructions.elementAt(bb.getEndAddress());
+							short instr_type = InstructionTable.InstructionTypeTable[instr.getOpcode()];
+							if (instr_type == InstructionTable.CONDITIONAL_INSTRUCTION) {
+								instr.addBefore("DynamicTool", "setBranchPC", new Integer(instr.getOffset()));
+								instr.addBefore("DynamicTool", "updateBranchNumber", new Integer(k));
+								instr.addBefore("DynamicTool", "updateBranchOutcome", "BranchOutcome");
+								k++;
+							}
+						}
+					}
+					ci.addBefore("DynamicTool", "setBranchClassName", ci.getClassName());
+					ci.addBefore("DynamicTool", "branchInit", new Integer(total));
+					ci.addAfter("DynamicTool", "printBranch", "null");
+					ci.write(out_filename);
+				}
+			}	
+		}
+
+	public static synchronized void setBranchClassName(String name)
+		{
+			branch_class_name = name;
+		}
+
+	public static synchronized void setBranchMethodName(String name) 
+		{
+			branch_method_name = name;
+		}
+	
+	public static synchronized void setBranchPC(int pc)
+		{
+			branch_pc = pc;
+		}
+	
+	public static synchronized void branchInit(int n) 
+		{
+			if (branch_info == null) {
+				branch_info = new StatisticsBranch[n];
+			}
+		}
+
+	public static synchronized void updateBranchNumber(int n)
+		{
+			branch_number = n;
+			
+			if (branch_info[branch_number] == null) {
+				branch_info[branch_number] = new StatisticsBranch(branch_class_name, branch_method_name, branch_pc);
+			}
+		}
+
+	public static synchronized void updateBranchOutcome(int br_outcome)
+		{
+			if (br_outcome == 0) {
+				branch_info[branch_number].incrNotTaken();
+			}
+			else {
+				branch_info[branch_number].incrTaken();
+			}
+		}
+
+	public static synchronized void printBranch(String foo)
+		{
+			System.out.println("Branch summary:");
+			System.out.println("CLASS NAME" + '\t' + "METHOD" + '\t' + "PC" + '\t' + "TAKEN" + '\t' + "NOT_TAKEN");
+			
+			for (int i = 0; i < branch_info.length; i++) {
+				if (branch_info[i] != null) {
+					System.out.println(branch_info[i].print());
+				}
+			}
+		}
+
+
+	private static class StatisticsBranch{ 
+		private StatisticsBranch(String class_name, String method_name, int pc) 
+			{
+				class_name_ = class_name;
+				method_name_ = method_name;
+				pc_ = pc;
+				taken_ = 0;
+				not_taken_ = 0;
+			}
+
+		private String print() 
+			{
+				return class_name_ + '\t' + method_name_ + '\t' + pc_ + '\t' + taken_ + '\t' + not_taken_;
+			}
+		
+		private void incrTaken()
+			{
+				taken_++;
+			}
+
+		private void incrNotTaken() 
+			{
+				not_taken_++;
+			}
+	}
 }
