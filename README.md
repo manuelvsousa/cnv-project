9d4b15298a85d38e74b376be60bd1b55a0d753ca
Kevin <kevindevos9542@gmail.com>
2019-04-24 12:23:15 +0100

    Compile script with instructions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cnv-project
+
+### Instructions
+##### Compiling
+Run in terminal:
+
+```
+bash compile.sh <path_to_AWS_SDK_directory>
+```
+
+Note that the sdk directory should have the folders `aws-java-sdk-<version>/lib`
+and `aws-java-sdk-<version>/third-party` as provided by the downloaded and unzipped sdk from amazon.
--- compile.sh
+++ compile.sh
@@ -1,11 +1,18 @@
-AWS_SDK_CLASSPATH="aws/lib/aws-java-sdk-1.11.536.jar"
+if [ $# -ne 1 ]; then
+	echo "Invalid number of commandline arguments. Pass the aws classpath directory as the first argument without ending '/' character"
+	exit 1
+fi
+
+AWS_SDK_CLASSPATH=$1"/lib/aws-java-sdk"*[0-9].jar # only get the main jar, only one ending in the version
+LIB_CLASSPATH="lib/*"
+CP=$(echo lib/*.jar | tr ' ' ':'):$(echo $AWS_SDK_CLASSPATH)
 
 rm -r build
 mkdir build
 
 export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
-javac -cp $AWS_SDK_CLASSPATH $(find . -name "*.java") -d build
+javac -cp $CP $(find . -name "*.java") -d build
 java -cp build BitTool build/pt/ulisboa/tecnico/cnv/solver build/pt/ulisboa/tecnico/cnv/solver
-java -cp build pt.ulisboa.tecnico.cnv.webserver.DynamicTool build/pt/ulisboa/tecnico/cnv/solver build/pt/ulisboa/tecnico/cnv/solver
+#java -cp build pt.ulisboa.tecnico.cnv.webserver.DynamicTool build/pt/ulisboa/tecnico/cnv/solver build/pt/ulisboa/tecnico/cnv/solver
 
 
