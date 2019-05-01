161d43e1fc7c57d949ecf06c0ddcdf2b4a181f26
Kevin <kevindevos9542@gmail.com>
2019-05-01 19:21:02 +0100

    Fix server ports

--- loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/LoadBalancer.java
+++ loadbalancer/src/pt/ulisboa/tecnico/cnv/loadbalancer/LoadBalancer.java
@@ -23,8 +23,9 @@ public class LoadBalancer {
 	private static MSSClient mssClient = new MSSClient("127.0.0.1", 8001);
 
 	public static void main(final String[] args) throws Exception {
-		final HttpServer server = HttpServer.create(new InetSocketAddress("127.0.0.1", 8000), 0);
-		//final HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+		//final HttpServer server = HttpServer.create(new InetSocketAddress("127.0.0.1", 8000), 0);
+		final HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
 		server.createContext("/climb", new ClimbHandler());
 		server.createContext("/requestFinishedProcessing", new PostMetricDataHandler());
 
