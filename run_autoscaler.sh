1280db81b7bb83a0313b5e917f29df404f9481ed
EC2 Default User <ec2-user@ip-172-31-37-54.ec2.internal>
2019-05-23 00:54:42 +0000

    Add autoscaler run script

---
+++ run_autoscaler.sh
@@ -0,0 +1,10 @@
+if [ $# -ne 1 ]; then
+	echo "Invalid number of commandline arguments. Pass the aws classpath directory as the first argument without ending '/' character"
+	exit 1
+fi
+
+export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
+
+CP=$(echo lib/*.jar | tr ' ' ':'):$(echo $1/lib/*.jar | tr ' ' ':'):$(echo $1/third-party/lib/*.jar | tr ' ' ':'):build
+
+java -cp $CP pt.ulisboa.tecnico.cnv.autoscaler.AutoScaler
